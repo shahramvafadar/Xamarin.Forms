ce550d66bf6be2701580e30bde994d6954598dd1
Rui Marinho <me@ruimarinho.net>
2019-05-02 15:26:42 +0100

    [Build]Remove extra AssemblyInfo on uwp and design csproj

--- Xamarin.Forms.Maps.UWP/Properties/AssemblyInfo.cs
+++ Xamarin.Forms.Maps.UWP/Properties/AssemblyInfo.cs
@@ -4,5 +4,3 @@ using Xamarin.Forms.Maps.UWP;
 using Xamarin.Forms.Platform.UWP;
 
 [assembly: ExportRenderer(typeof (Map), typeof (MapRenderer))]
-[assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.0.0.0")]
